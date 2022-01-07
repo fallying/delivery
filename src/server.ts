5f4fe0ff62c39d5b4b2b49200cad63090bb8ada7
Jonas Brito <jonasexplore@gmail.com>
2022-01-07 17:14:37 -0300

    chore(*): import routes and interceptors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
+import "express-async-errors";
+
 import express from "express";
+import { interceptorError } from "./infra/interceptors";
+import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use(interceptorError);
 
 const PORT = process.env.PORT || 3333;
 
